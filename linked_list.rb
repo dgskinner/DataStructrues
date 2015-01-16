701fdfaadac95fa570581066984343020d2624f4
Duncan Skinner <skinnerduncan@gmail.com>
2015-01-15 16:33:32 -0800

    first commit

---
+++ linked_list.rb
@@ -0,0 +1,29 @@
+class Link
+  def initialize (value, next, prev)
+    @value = value
+    @next = next
+    @prev = prev
+  end
+end
+
+class LinkedList
+  def initialize (first_link) 
+    @first = first_link
+  end
+  
+  def insert (link)
+    
+  end
+  
+  def remove
+    
+  end
+  
+  def []
+    
+  end
+  
+  def []= (index)
+    
+  end
+end
